6255a0d7b24bc6ddbe0427dd9ba73cbe7d9ace46
thyeone <thyeonee@gmail.com>
2025-01-13 11:56:06 +0900

    plugin 제외

--- packages/plugin-prettier/src/index.ts
+++ packages/plugin-prettier/src/index.ts
@@ -6,17 +6,26 @@ import type { Plugin } from '@svgr/core'
 const prettierPlugin: Plugin = (code, config, state) => {
   if (!config.prettier) return code
   const filePath = state.filePath || process.cwd()
+
+  // prettier 설정을 가져올 때 plugins 제외
   const prettierRcConfig = config.runtimeConfig
-    ? resolveConfig.sync(filePath, { editorconfig: true })
+    ? {
+        ...resolveConfig.sync(filePath, { editorconfig: true }),
+        plugins: [], // plugins 비우기
+      }
     : {}
-  return format(
-    code,
-    deepmerge.all([
-      { parser: 'babel' },
-      prettierRcConfig || {},
-      config.prettierConfig || {},
-    ]),
-  )
+
+  // 최종 설정에서도 plugins 제외 보장
+  const finalConfig = deepmerge.all([
+    { parser: 'babel' },
+    prettierRcConfig || {},
+    {
+      ...(config.prettierConfig || {}),
+      plugins: [], // plugins 비우기
+    },
+  ])
+
+  return format(code, finalConfig)
 }
 
 export default prettierPlugin
